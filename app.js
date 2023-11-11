5f84150023a670060665327ef06348cff1d499e0
Juan Cruz <j.cruzb@outlook.com>
2023-11-11 16:51:57 +0100

    Open Server

---
+++ app.js
@@ -0,0 +1,54 @@
+require("dotenv").config();
+// Importar librerías
+const express = require('express');
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const createError = require("http-errors");
+const { StatusCodes } = require("http-status-codes");
+const jwt = require("jsonwebtoken");
+const cors = require("cors");
+
+require("./config/db.config");
+
+const app = express();
+
+//cors
+app.use(
+    cors({
+      origin: process.env.CORS_ORIGIN || ["http://localhost:5173", "http://127.0.0.1:5173"],
+    })
+  );
+
+  //configurar el servidor
+app.use(logger("dev"));
+app.use(express.json());
+
+//ruta inicial
+
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to the API" });
+  });
+
+
+//rutas
+
+//route not found
+app.use((req, res, next) => {
+    next(createError(StatusCodes.NOT_FOUND, "Route not found"));
+  });
+  
+  //error handler
+  app.use((error, req, res, next) => {
+    res.status(error.status || StatusCodes.INTERNAL_SERVER_ERROR);
+    res.json({ error: error.message });
+  });
+  
+//listen
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
+
+
+
+
