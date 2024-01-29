de4df1f5043e1abe8cbfc686a6acca76fdef3a64
Juan Cruz <j.cruzb@outlook.com>
2024-01-29 22:25:24 +0100

    updating cors

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const app = express();
 //cors
 app.use(
   cors({
-    origin: process.env.CORS_ORIGINJC || ["http://127.0.0.1:5173", "http://localhost:5173"]
+    origin: process.env.CORS_ORIGINJC || ["http://127.0.0.1:5173", "http://localhost:5173"],
   })
 );
 // app.use(
