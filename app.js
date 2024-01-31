844ffd9b28f577f43d1e68f61962f04ccde93c11
Juan Cruz <j.cruzb@outlook.com>
2024-01-31 23:07:44 +0100

    updating cors

--- app.js
+++ app.js
@@ -13,11 +13,7 @@ require("./config/db.config");
 const app = express();
 
 
-app.use(cors(
-  {
-    origin: process.env.CORS_ORIGINJC || ["http://localhost:5173", "http://127.0.0.1:5173"],
-  }
-))
+app.use(cors())
 
 console.log(process.env.CORS_ORIGINJC)
 // app.use(
